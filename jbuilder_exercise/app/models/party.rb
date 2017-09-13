720d80a98f2e09057767a4c0822594371df72ba3
App Academy Student <student@appacademy.io>
2017-09-13 11:43:17 -0700

    Initial commit finish jbuilder exercise

---
+++ jbuilder_exercise/app/models/party.rb
@@ -0,0 +1,10 @@
+class Party < ApplicationRecord
+  validates :name, :location, presence: true
+
+  has_many :invitations
+
+  has_many :guests,
+    through: :invitations,
+    source: :guest
+
+end
